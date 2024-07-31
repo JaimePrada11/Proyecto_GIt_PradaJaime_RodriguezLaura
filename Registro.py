0286c1ecff54297f401c5c4a7c69b24d08ea64cc
JaimePrada11 <jandrespradato.11@gmail.com>
2024-07-31 11:42:34 -0500

    feat: :sparkles: Agrego Funcion de Validacion de Codigo postal
    Se agrego la funcion de validar que solo haya un codigo postal al momento de agregar una nueva ciudad para q no se repita al momento de usarse la funcion de registro_ciudades
    
    by @JaimePrada11

--- Registro.py
+++ Registro.py
@@ -4,8 +4,19 @@ def registro_ciudades():
     cargar_datos()
     data = {}
     Codigo_Postal = input("Ingresa el codigo postal de la ciudad:  ")
-    data["Nombre"] = input("Ingresa el nombre de la ciudad: ").upper()
-    data["Poblacion"] = int(input("Ingresa la poblacion estimada de la ciudad: "))
-    data["Pais"] = input("Ingresa el pais de la ciudad: ").upper()
-    Ciudades[Codigo_Postal] = data
+    if not validar_codigo(Codigo_Postal):
+        data["Nombre"] = input("Ingresa el nombre de la ciudad: ").upper()
+        data["Poblacion"] = int(input("Ingresa la poblacion estimada de la ciudad: "))
+        data["Pais"] = input("Ingresa el pais de la ciudad: ").upper()
+        Ciudades[Codigo_Postal] = data
+    else:
+        print("La ciudad ya existe")
     guardar_datos()
+
+
+def validar_codigo(codigo):
+    for Categoria in Ciudades.keys():
+        if codigo in Categoria:
+            return True
+    return False
+
